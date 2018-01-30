cd77071907e0206498b6483e7b713915d1b7388c
Maestro <Maestro@imac-de-maestro-10.local>
2018-01-29 18:01:42 -0700

    primera version

--- transitionTest/ViewController.swift
+++ transitionTest/ViewController.swift
@@ -9,7 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var lbl_contador: UILabel!
+    @IBOutlet weak var vw_LabelContainer: UIView!
+    var contador = 0;
 
+    @IBAction func tap_Transition(_ sender: Any) {
+        
+        let random = round(Double(drand48()*6))
+        
+        switch(random){
+        case 0:
+            UIView.transition(with: vw_LabelContainer, duration: 0.5, options: [.transitionCurlUp], animations: {
+                self.oneUp()
+            }, completion: nil)
+            break;
+        case 1:
+            UIView.transition(with: vw_LabelContainer, duration: 0.5, options: [.transitionCurlDown], animations: {
+                self.oneUp()
+            }, completion: nil)
+            break;
+        case 2:
+            UIView.transition(with: vw_LabelContainer, duration: 0.5, options: [.transitionFlipFromTop], animations: {
+                self.oneUp()
+            }, completion: nil)
+            break;
+        case 3:
+            UIView.transition(with: vw_LabelContainer, duration: 0.5, options: [.transitionFlipFromLeft], animations: {
+                self.oneUp()
+            }, completion: nil)
+            break;
+        case 4:
+            UIView.transition(with: vw_LabelContainer, duration: 0.5, options: [.transitionCrossDissolve], animations: {
+                self.oneUp()
+            }, completion: nil)
+            break;
+        case 5:
+            UIView.transition(with: vw_LabelContainer, duration: 0.5, options: [.transitionFlipFromRight], animations: {
+                self.oneUp()
+            }, completion: nil)
+            break;
+        case 6:
+            UIView.transition(with: vw_LabelContainer, duration: 0.5, options: [.transitionFlipFromBottom], animations: {
+                self.oneUp()
+            }, completion: nil)
+            break;
+        default:
+            break;
+        }
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +72,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func oneUp(){
+        self.contador += 1
+        self.lbl_contador.text = String(self.contador)
+    }
 
+    @IBAction func toLeft(_ sender: Any) {
+        UIView.transition(with: vw_LabelContainer, duration: 0.5, options: [.transitionFlipFromRight], animations: {
+            self.oneUp()
+        }, completion: nil)
+    }
+    
+    @IBAction func toRight(_ sender: Any) {
+        UIView.transition(with: vw_LabelContainer, duration: 0.5, options: [.transitionFlipFromLeft], animations: {
+            self.oneUp()
+        }, completion: nil)
+    }
+   
+    @IBAction func toTop(_ sender: Any) {
+        UIView.transition(with: vw_LabelContainer, duration: 0.5, options: [.transitionFlipFromTop], animations: {
+            self.oneUp()
+        }, completion: nil)
+    }
+    
+    @IBAction func toDown(_ sender: Any) {
+        UIView.transition(with: vw_LabelContainer, duration: 0.5, options: [.transitionFlipFromBottom], animations: {
+            self.oneUp()
+        }, completion: nil)
+    }
 }
 
